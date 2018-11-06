cefc396b216d415ad546ca9539bdd164b29caeb3
Daniel Dadap <daniel@dadap.net>
2018-11-06 04:22:59 +0000

    choH wa'DIch
    wej vay' ta'laH

---
+++ chabal-tetlh.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Plugin name: chabal tetlh
+ * Description: mu' chu' tIchup. mu' chu' tIwIv.
+ * Version: 0.0
+ * Author: De'nIl maqel puqloD DapDap tuq
+ * Author URI: https://github.com/dadap
+ *
+ *                                nIqHom mab quv
+ *
+ * nIqHomvam lulo' Hoch net chaw'. meq wIvlaH lo'wI'. nIqHom choHlu' net chaw'
+ * je. nIqHomvam velqa' chenmoHlu' net chaw' je 'ej velqa' lInlu' net chaw'.
+ * lInlu'chugh, tu'qom wIvlaH lInwI'. Hoch chawlu', 'ach cha' chut pabnISlu':
+ *
+ *     wa':  reH nIqHomvam tlhejnIS mabvam. mabvam teqlu' net tuch.
+ *     cha': potlh mabvam mu'mey.  mabvam choHlu' net tuch.
+ *
+ * nIqHomvam lupeS neH nIqHom peSwI'pu': lo'wI' ngoQvaD mIt 'oH 'e' lay'be'lu'.
+ * nIqHomvam lo'lu'taHvIS qaSchugh vay', ngoy' nIqHom lo'wI' neH.
+ *
+ *                       The honorable software contract
+ *
+ * It is permitted for all to use this software. A user may choose the reason.
+ * It is also permitted that the software be altered. It is also permitted that
+ * replicas of this software be created, and it is permitted that replicas be
+ * shared. If it is shared, the one who shares may choose the form. All is
+ * permitted, but two laws must be followed:
+ *
+ *     one:  This contract must always accompany this software. It is forbidden
+ *           to remove this contract.
+ *     two:  The words of this contract are important. It is forbidden to alter
+ *           this contract.
+ *
+ * The suppliers of the software only supply it: It is not promised that it be
+ * suitable for the user's purpose. If something occurs while this software is
+ * being used, only the user of the software is responsible.
+ *
+ */
+
+function chabal_tISuq() {
+?>
+    <form>
+        <input type="text" />
+    </form>
+<?php
+}
+add_shortcode('chabal_tetlh', 'chabal_tISuq');
+?>
